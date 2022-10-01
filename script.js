7eaa97b5149e07924207b38aa817ea2149d87a6d
Alejo <alejosegi@gmail.com>
2022-10-02 01:16:28 +0200

    Creating the project game

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function () {
+  // canvas setup
+  const canvas = this.document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 500;
+  canvas.height = 500;
+
+  class InputHandler {}
+
+  class Projectile {}
+
+  class Particle {}
+
+  class Player {}
+
+  class Enemy {}
+
+  class Layer {}
+
+  class Bacground {}
+
+  class UI {}
+
+  class game {}
+});

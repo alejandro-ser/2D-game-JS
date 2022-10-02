8a36c8dfd1c689e9a3782085d47e4475f04a1f7e
Alejo <alejosegi@gmail.com>
2022-10-03 01:00:53 +0200

    Win and lose condition

--- script.js
+++ script.js
@@ -151,11 +151,35 @@ window.addEventListener("load", function () {
       context.font = this.fontSize + "px " + this.fontFamily;
       // score
       context.fillText("Score: " + this.game.score, 20, 40);
-
       // ammo
       context.fillStyle = this.color;
       for (let i = 0; i < this.game.ammo; i++) {
         context.fillRect(20 + 5 * i, 50, 3, 20);
+      }
+      // game over messages
+      if (this.game.gameOver) {
+        context.textAlign = "center";
+        let message1;
+        let message2;
+        if (this.game.score > this.game.winningScore) {
+          message1 = "You win!";
+          message2 = "Well done!";
+        } else {
+          message1 = "You lose!";
+          message2 = "Try again next time!";
+        }
+        context.font = "50px " + this.fontFamily;
+        context.fillText(
+          message1,
+          this.game.width * 0.5,
+          this.game.height * 0.5 - 40
+        );
+        context.font = "25px " + this.fontFamily;
+        context.fillText(
+          message2,
+          this.game.width * 0.5,
+          this.game.height * 0.5 + 40
+        );
       }
       context.restore();
     }

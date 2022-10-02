9788f02445f5b1c1f6b2b1e5a179ab3b30640687
Alejo <alejosegi@gmail.com>
2022-10-02 11:33:14 +0200

    Using OOP with JS

--- script.js
+++ script.js
@@ -1,25 +1,73 @@
 window.addEventListener("load", function () {
   // canvas setup
-  const canvas = this.document.getElementById("canvas1");
+  const canvas = document.getElementById("canva1");
   const ctx = canvas.getContext("2d");
-  canvas.width = 500;
+  canvas.width = 1500;
   canvas.height = 500;
 
-  class InputHandler {}
+  class InputHandler {
+    constructor(game) {
+      this.game;
+    }
+  }
 
   class Projectile {}
 
   class Particle {}
 
-  class Player {}
+  class Player {
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 199;
+      this.x = 20;
+      this.y = 100;
+      this.speedY = 1;
+    }
+
+    update() {
+      this.y += this.speedY;
+    }
+
+    draw(context) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
 
   class Enemy {}
 
   class Layer {}
 
-  class Bacground {}
+  class Background {}
 
   class UI {}
 
-  class game {}
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+      this.input = new InputHandler(this);
+      this.keys = [];
+    }
+
+    update() {
+      this.player.update();
+    }
+
+    draw(context) {
+      this.player.draw(context);
+    }
+  }
+
+  const game = new Game(canvas.width, canvas.height);
+
+  // animation loop
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.update();
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
+  animate();
 });

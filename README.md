613adfe2edacc213a64f943df019eb3ef174d736
4l3j0 <alejosegi@gmail.com>
2022-10-05 22:16:11 +0200

    Animated parallax backgrounds

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 2D-game-JS
+
 2D game with JavaScript using OOP on HTML canvas
+
+### Based on an [FreeCodeCamp](https://www.freecodecamp.org/) tutorial
+
+[Code a 2D Game Using JavaScript, HTML, and CSS (w/ Free Game Assets)](https://youtu.be/7BHs1BzA4fs)
--- script.js
+++ script.js
@@ -130,9 +130,50 @@ window.addEventListener("load", function () {
     }
   }
 
-  class Layer {}
+  class Layer {
+    constructor(game, image, speedModifier) {
+      this.game = game;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.width = 1768;
+      this.height = 500;
+      this.x = 0;
+      this.y = 0;
+    }
 
-  class Background {}
+    update() {
+      if (this.x <= -this.width) this.x = 0;
+      this.x -= this.game.speed * this.speedModifier;
+    }
+
+    draw(context) {
+      context.drawImage(this.image, this.x, this.y);
+      context.drawImage(this.image, this.x + this.width, this.y);
+    }
+  }
+
+  class Background {
+    constructor(game) {
+      this.game = game;
+      this.image1 = document.getElementById("layer1");
+      this.image2 = document.getElementById("layer2");
+      this.image3 = document.getElementById("layer3");
+      this.image4 = document.getElementById("layer4");
+      this.layer1 = new Layer(this.game, this.image1, 0.2);
+      this.layer2 = new Layer(this.game, this.image2, 0.4);
+      this.layer3 = new Layer(this.game, this.image3, 1);
+      this.layer4 = new Layer(this.game, this.image4, 1.5);
+      this.layers = [this.layer1, this.layer2, this.layer3];
+    }
+
+    update() {
+      this.layers.forEach((layer) => layer.update());
+    }
+
+    draw(context) {
+      this.layers.forEach((layer) => layer.draw(context));
+    }
+  }
 
   class UI {
     constructor(game) {
@@ -191,6 +232,7 @@ window.addEventListener("load", function () {
     constructor(width, height) {
       this.width = width;
       this.height = height;
+      this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler(this);
       this.ui = new UI(this);
@@ -207,11 +249,14 @@ window.addEventListener("load", function () {
       this.winningScore = 10;
       this.gameTime = 0;
       this.timeLimit = 15000;
+      this.speed = 1;
     }
 
     update(deltaTime) {
       if (!this.gameOver) this.gameTime += deltaTime;
       if (this.gameTime > this.timeLimit) this.gameOver = true;
+      this.background.update();
+      this.background.layer4.update();
       this.player.update();
       if (this.ammoTimer > this.ammoInterval) {
         if (this.ammo < this.maxAmmo) this.ammo++;
@@ -249,11 +294,13 @@ window.addEventListener("load", function () {
     }
 
     draw(context) {
+      this.background.draw(context);
       this.player.draw(context);
       this.ui.draw(context);
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
       });
+      this.background.layer4.draw(context);
     }
 
     addEnemy() {

20e9f5210b1760b6b1e78dd657a152cdac158bde
Alejo <alejosegi@gmail.com>
2022-10-08 09:55:06 +0200

    Creating a debug mode

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ window.addEventListener("load", function () {
           this.game.keys.push(e.key);
         } else if (e.key === " ") {
           this.game.player.shootTop();
+        } else if (e.key === "d") {
+          this.game.debug = !this.game.debug;
         }
       });
 
@@ -90,8 +92,8 @@ window.addEventListener("load", function () {
     }
 
     draw(context) {
-      context.fillStyle = "black";
-      context.fillRect(this.x, this.y, this.width, this.height);
+      if (this.game.debug)
+        context.strokeRect(this.x, this.y, this.width, this.height);
       context.drawImage(
         this.image,
         this.frameX * this.width,
@@ -271,6 +273,7 @@ window.addEventListener("load", function () {
       this.gameTime = 0;
       this.timeLimit = 15000;
       this.speed = 1;
+      this.debug = true;
     }
 
     update(deltaTime) {

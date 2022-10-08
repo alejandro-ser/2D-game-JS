29cb5f6e9cc8db7b78e9ca20d55e093a175ddc6a
Alejo <alejosegi@gmail.com>
2022-10-08 10:41:14 +0200

    Animating enemy sprites, night and lucky fisch

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
   // canvas setup
   const canvas = document.getElementById("canva1");
   const ctx = canvas.getContext("2d");
-  canvas.width = 1500;
+  canvas.width = 700;
   canvas.height = 500;
 
   class InputHandler {
@@ -126,19 +126,34 @@ window.addEventListener("load", function () {
       this.x = this.game.width;
       this.speedX = Math.random() * -1.5 - 0.5;
       this.markedForDeletion = false;
-      this.lives = 5;
-      this.score = this.lives;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrame = 37;
     }
 
     update() {
-      this.x += this.speedX;
+      this.x += this.speedX - this.game.speed;
       if (this.x + this.width < 0) this.markedForDeletion = true;
+      // sprite animation
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else this.frameX = 0;
     }
 
     draw(context) {
-      context.fillStyle = "red";
-      context.fillRect(this.x, this.y, this.width, this.height);
-      context.fillStyle = "black";
+      if (this.game.debug)
+        context.strokeRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
       context.font = "20px Helvetica";
       context.fillText(this.lives, this.x, this.y);
     }
@@ -147,9 +162,40 @@ window.addEventListener("load", function () {
   class Angler1 extends Enemy {
     constructor(game) {
       super(game);
-      this.width = 228 * 0.2;
-      this.height = 169 * 0.2;
+      this.width = 228;
+      this.height = 169;
       this.y = Math.random() * (this.game.height * 0.9 - this.height);
+      this.image = document.getElementById("angler1");
+      this.frameY = Math.floor(Math.random() * 3);
+      this.lives = 2;
+      this.score = this.lives;
+    }
+  }
+
+  class Angler2 extends Enemy {
+    constructor(game) {
+      super(game);
+      this.width = 213;
+      this.height = 165;
+      this.y = Math.random() * (this.game.height * 0.9 - this.height);
+      this.image = document.getElementById("angler2");
+      this.frameY = Math.floor(Math.random() * 2);
+      this.lives = 3;
+      this.score = this.lives;
+    }
+  }
+
+  class LuckyFisch extends Enemy {
+    constructor(game) {
+      super(game);
+      this.width = 99;
+      this.height = 95;
+      this.y = Math.random() * (this.game.height * 0.9 - this.height);
+      this.image = document.getElementById("lucky");
+      this.frameY = Math.floor(Math.random() * 2);
+      this.lives = 3;
+      this.score = 15;
+      this.type = "lucky";
     }
   }
 
@@ -328,7 +374,10 @@ window.addEventListener("load", function () {
     }
 
     addEnemy() {
-      this.enemies.push(new Angler1(this));
+      const randomize = Math.random();
+      if (randomize < 0.5) this.enemies.push(new Angler1(this));
+      else if (randomize < 0.6) this.enemies.push(new Angler2(this));
+      else this.enemies.push(new LuckyFisch(this));
       console.log(this.enemies);
     }
 

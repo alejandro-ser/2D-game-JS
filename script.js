58ffc7128b3ffd2e2aa204788efda714f55a66c6
Alejo <alejosegi@gmail.com>
2022-10-08 09:42:21 +0200

    Sprite animation with JavaScript

--- script.js
+++ script.js
@@ -55,12 +55,16 @@ window.addEventListener("load", function () {
     constructor(game) {
       this.game = game;
       this.width = 120;
-      this.height = 199;
+      this.height = 190;
       this.x = 20;
       this.y = 100;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrame = 37;
       this.speedY = 0;
       this.maxSpeed = 3;
       this.projectiles = [];
+      this.image = document.getElementById("player");
     }
 
     update() {
@@ -77,11 +81,28 @@ window.addEventListener("load", function () {
       this.projectiles = this.projectiles.filter(
         (projectile) => !projectile.markedForDeletion
       );
+      // sprite animation
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else {
+        this.frameX = 0;
+      }
     }
 
     draw(context) {
       context.fillStyle = "black";
       context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
       this.projectiles.forEach((projectile) => {
         projectile.draw(context);
       });
